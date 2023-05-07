5f8bee7a4bb133cd1d9faccd40e56fed11f76c5a
elderlira <elderfranca@hotmail.com>
2023-05-07 20:27:17 -0300

    created service to create and delete think

--- src/app/components/pensamentos/service/pensamento.service.ts
+++ src/app/components/pensamentos/service/pensamento.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Pensamento } from '../interface/pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
+private readonly API = 'http://localhost:3000/pensamento'
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {}
 
+  listar(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.API)
+  }
+
+  criarPensamento(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API, pensamento)
+  }
+
+  deletarPensamento(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`
+    return this.http.delete<Pensamento>(url)
   }
 }
