2f51cf794ada21acc307a73f25d758572a91a679
elderlira <elderfranca@hotmail.com>
2023-05-08 16:33:29 -0300

    added atualizar method and verify parametry id

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,6 +1,7 @@
+import { Pensamento } from './../interface/pensamento';
 import { Component } from '@angular/core';
 import { PensamentoService } from '../service/pensamento.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,7 +10,8 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent {
 
-  pensamento = {
+  pensamento :Pensamento =  {
+    id: 0,
     conteudo: '',
     autoria: '',
     modelo: 'modelo3'
@@ -17,14 +19,33 @@ export class CriarPensamentoComponent {
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
     ) {}
+
+  ngOnInit() {
+      const id = this.route.snapshot.paramMap.get('id')
+      if(!id) {
+        return
+      }
+      this.service.buscarPorId(parseInt(id!)).subscribe((p)=> {
+        this.pensamento = p
+      })
+    }
 
   criarPensamento() {
     alert('Pensamento registrado')
     this.service.criarPensamento(this.pensamento).subscribe(()=> {
       this.router.navigate(['/listarPensamento'])
     })
+  }
+
+  atualizar() {
+    if(this.pensamento.id) {
+      this.service.atualizarPensamento(this.pensamento.id, this.pensamento).subscribe(()=> {
+        this.service.retornar()
+      })
+    }
   }
 
   cancelarPensamento(): void {
