db4369cbceb4e8465914e1185ddc23d3b9c76a0c
elderlira <elderfranca@hotmail.com>
2023-05-08 11:49:16 -0300

    created buscarPorId, excluirPensamento and retornar service

--- src/app/components/pensamentos/service/pensamento.service.ts
+++ src/app/components/pensamentos/service/pensamento.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Pensamento } from '../interface/pensamento';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,11 @@ export class PensamentoService {
 
 private readonly API = 'http://localhost:3000/pensamento'
 
-  constructor(private http: HttpClient) {}
-
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    ) {}
+  
   listar(): Observable<Pensamento[]> {
     return this.http.get<Pensamento[]>(this.API)
   }
@@ -20,8 +24,15 @@ private readonly API = 'http://localhost:3000/pensamento'
     return this.http.post<Pensamento>(this.API, pensamento)
   }
 
-  deletarPensamento(id: number): Observable<Pensamento> {
-    const url = `${this.API}/${id}`
-    return this.http.delete<Pensamento>(url)
+  buscarPorId(id: number) {
+    return this.http.get<Pensamento>(`${this.API}/${id}`)
+  }
+
+  excluirPensamento(id: number) {
+    return this.http.delete<Pensamento>(`${this.API}/${id}`)
+  }
+
+  retornar(): void {
+    this.router.navigate(['/listarPensamento'])
   }
 }
