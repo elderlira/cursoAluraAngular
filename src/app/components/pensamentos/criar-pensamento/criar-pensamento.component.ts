239ce72edc41eb53cbfa3a525e75ceeb81b1d4c3
elderlira <elderfranca@hotmail.com>
2023-05-14 21:22:23 -0300

    added formBuild and validators fields

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,8 @@ import { Pensamento } from './../interface/pensamento';
 import { Component } from '@angular/core';
 import { PensamentoService } from '../service/pensamento.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { lowerCaseValidator } from './newValidator';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -17,32 +19,62 @@ export class CriarPensamentoComponent {
     modelo: 'modelo3'
   }
 
+  formulario!: FormGroup;
+
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
     ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      id: [this.route.snapshot.paramMap.get('id')],
+      conteudo: ['', [Validators.required]],
+      autoria: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required,
+        Validators.maxLength(6),
+        lowerCaseValidator
+      ])],
+      modelo: ['', Validators.required]
+    }) 
       const id = this.route.snapshot.paramMap.get('id')
       if(!id) {
         return
       }
+
       this.service.buscarPorId(parseInt(id!)).subscribe((p)=> {
         this.pensamento = p
+        this.formulario = this.formBuilder.group({
+          id: [this.route.snapshot.paramMap.get('id')],
+          conteudo: [p.conteudo, [Validators.required]],
+          autoria: [p.autoria, Validators.compose([
+            Validators.minLength(3),
+            Validators.required,
+            Validators.maxLength(6),
+            lowerCaseValidator
+          ])],
+          modelo: [p.modelo, Validators.required]
+          
+        })
       })
+
     }
 
   criarPensamento() {
-    alert('Pensamento registrado')
-    this.service.criarPensamento(this.pensamento).subscribe(()=> {
-      this.router.navigate(['/listarPensamento'])
-    })
+    if(this.formulario.valid) {
+      alert('Pensamento registrado')
+      this.service.criarPensamento(this.formulario.value).subscribe(()=> {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   atualizar() {
-    if(this.pensamento.id) {
-      this.service.atualizarPensamento(this.pensamento.id, this.pensamento).subscribe(()=> {
+    if(this.formulario.value.id) {
+      this.service.atualizarPensamento(this.formulario.value.id, this.formulario.value).subscribe(()=> {
         this.service.retornar()
       })
     }
