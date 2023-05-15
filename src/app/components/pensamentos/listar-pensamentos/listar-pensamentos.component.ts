3854df9fc51138b653dc4445c66c1bd9ae335a95
elderlira <elderfranca@hotmail.com>
2023-05-15 11:31:28 -0300

    added verificarNovasMensagens method

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -10,16 +10,29 @@ import { PensamentoService } from '../service/pensamento.service';
 export class ListarPensamentosComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = []
+  paginaAtual: number = 1;
+  haMaisMensagem: boolean = true
 
-  constructor(private service: PensamentoService) {
-
-
-  }
+  constructor(
+    private service: PensamentoService
+    ) {}
 
   ngOnInit(): void { 
-    this.service.listar().subscribe((lista)=> {
+    this.service.listar(this.paginaAtual).subscribe((lista)=> {
       this.listaPensamentos = lista;
     })
   }
 
+  verificarNovasMensagens() {
+    this.service.listar(++this.paginaAtual).subscribe(lista => {
+      this.listaPensamentos.push(...lista)
+      
+      if(!lista.length) {
+        this.haMaisMensagem = false
+      }
+
+    })
+    
+    
+  }
 }
